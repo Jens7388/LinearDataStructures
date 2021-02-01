f56b006257bfb28e1390c7544fe562021fdc80df
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-02-01 14:58:54 +0100

    added stack

---
+++ DataStructures/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Stack<T> : LinearDataStructure<T>
+    {
+        private T[] array;
+
+        public Stack(int initialLength) : base(initialLength: initialLength)
+        {
+            array = new T[length];
+        }
+
+        public Push()
+        {
+
+        }
+
+        public virtual void Pop()
+        {
+
+        }
+
+    }
+}
