0f5066909ac2a14f1cd35198505a29a0141785a3
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-02-03 14:53:53 +0100

    Added linked list

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class LinkedList<T> : LinearDataStructure<Node<T>>
+    {
+        private bool isEmpty;
+
+        public LinkedList(int initialLength) : base (initialLength: initialLength)
+        {
+            length = initialLength;
+        }
+
+        public virtual void ReplaceFirst(Node<T> newNode)
+        {
+            if(IsEmpty)
+            {
+
+            }
+            else
+            {
+                Items[0] = newNode;
+            }
+        }
+
+        public virtual void ReplaceRest(LinkedList<T> newList)
+        {
+            if(IsEmpty)
+            {
+
+            }
+            else
+            {
+                int j = 0;
+                for(int i = 1; i < Count - 1; i++)
+                {
+                    Items[i] = newList.Items[j];
+                }
+            }
+        }
+
+        public virtual bool IsEmpty
+        {
+            get
+            {
+                return Count == 0;
+            }
+            set
+            {
+                isEmpty = value;
+            }
+        }    
+    }
+}
---
+++ DataStructures/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class Node<T>
+    {
+        protected T _data;
+        public Node(T data)
+        {
+            _data = data;
+        }
+    }
+}
